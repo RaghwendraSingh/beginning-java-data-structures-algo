18c9202adad2a457b433611b94de00d2d3ca8170
RaghwendraSingh <raghz1994@gmail.com>
2018-10-20 23:39:27 +0530

    corrected the function for binary to decimal conversion

--- src/main/org/raghwendra/dsa/lesson1/activity/binarytodecimal/BinaryToDecimal.java
+++ src/main/org/raghwendra/dsa/lesson1/activity/binarytodecimal/BinaryToDecimal.java
@@ -3,16 +3,14 @@ package org.raghwendra.dsa.lesson1.activity.binarytodecimal;
 class BinaryToDecimal {
 
 	public static int convertToDecimal(String binary) {
-		int conversion = 1;
-		int result = 0;
-		for(int i = 1; i <= binary.length() - 1; i++) {
-			if (binary.charAt(binary.length() - i) == '1') {
-				result += conversion;
-				conversion *= 2;
-			}
-		}
-
-		return result;
+	  int conversion = 1;
+	  int result = 0;
+	  for (int i = 1; i <= binary.length(); i++) {
+	    if (binary.charAt(binary.length() - i) == '1')
+	      result += conversion;
+	    conversion *= 2;
+	  }
+	  return result;
 	}
 
 	public static void main(String[] args) {
