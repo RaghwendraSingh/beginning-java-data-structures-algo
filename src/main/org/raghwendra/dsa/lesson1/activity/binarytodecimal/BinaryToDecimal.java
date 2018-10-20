65119ac1a6298f60283ea9014c9cb6e213834a20
RaghwendraSingh <raghz1994@gmail.com>
2018-10-20 23:22:18 +0530

    added function for convertToDecimal to convert binary to decimal

--- src/main/org/raghwendra/dsa/lesson1/activity/binarytodecimal/BinaryToDecimal.java
+++ src/main/org/raghwendra/dsa/lesson1/activity/binarytodecimal/BinaryToDecimal.java
@@ -1,7 +1,21 @@
-package org.raghwendra.dsa.lesson1.activity.binarytodecimal
+package org.raghwendra.dsa.lesson1.activity.binarytodecimal;
 
 class BinaryToDecimal {
-	public static void main(Array[String] args) {
-		
+
+	public static int convertToDecimal(String binary) {
+		int conversion = 1;
+		int result = 0;
+		for(int i = 1; i <= binary.length() - 1; i++) {
+			if (binary.charAt(binary.length() - i) == '1') {
+				result += conversion;
+				conversion *= 2;
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("110 is " + BinaryToDecimal.convertToDecimal("110"));
 	}
 }
